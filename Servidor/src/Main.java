dc2eff9618119a3cc84c8535c7f3064cc17aa859
caballerito22 <alecabnun@alu.edu.gva.es>
2025-02-23 00:02:04 +0100

    23-02 acabado y explicado

--- Servidor/src/Main.java
+++ Servidor/src/Main.java
@@ -8,11 +8,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
+        //reciclamos código del juego de piedra papel o tijera, ya que al principio hay que hacer lo mismo
         ServerSocket serverSocket = new ServerSocket(9090);
         System.out.println("Esperando jugadores... Un momento por favor.");
 
-        // Conexión de los jugadores
         Socket socket1 = serverSocket.accept();
         PrintWriter writer1 = new PrintWriter(socket1.getOutputStream(), true);
         BufferedReader reader1 = new BufferedReader(new InputStreamReader(socket1.getInputStream()));
@@ -21,7 +20,6 @@ public class Main {
         PrintWriter writer2 = new PrintWriter(socket2.getOutputStream(), true);
         BufferedReader reader2 = new BufferedReader(new InputStreamReader(socket2.getInputStream()));
 
-        // Mensaje de bienvenida y nombre de los jugadores
         writer1.println("Bienvenido al Juego del Nim!, estamos buscando otro jugador para empezar...");
         writer1.println();
         writer2.println("Bienvenido al Juego del Nim! ");
@@ -39,67 +37,79 @@ public class Main {
         writer1.println("El juego es al mejor de 3 partidas, es decir, el que gane dos partidas, será el ganador.");
         writer2.println("El juego es al mejor de 3 partidas, es decir, el que gane dos partidas, será el ganador.");
 
-        // Variables para el conteo de victorias
+        //eso es para contar las partidas de cada player
         int winsP1 = 0, winsP2 = 0;
 
-        // Juego principal
+        //esto se hará mientras ninguno de los dos haya ganado (no hayan llegado a dos victorias)
         while (winsP1 < 2 && winsP2 < 2) {
-            // Configuración del juego
+            //la forma del tablero h palos que tiene
             int[][] tablero = {
-                    {1, 1, 1, 1, 1}, // Montón 1 con 5 fichas (5 palos)
-                    {1, 1, 1, 1},     // Montón 2 con 4 fichas (4 palos)
-                    {1, 1, 1}         // Montón 3 con 3 fichas (3 palos)
+                    {1, 1, 1, 1, 1},
+                    {1, 1, 1, 1},
+                    {1, 1, 1}
             };
 
-            int currentPlayer = 1;
+            //empieza el jugador 1
+            int turnoJugador = 1;
 
-            // Mostrar el tablero al principio de cada partida
+            //mostramos el tablero
             showBoard(writer1, writer2, tablero);
 
-            // Juego de la partida
+            //while true para que se ejecute siempre hasta que no queden palos (se activa el breack)
             while (true) {
-                if (currentPlayer == 1) {
+                //cuando le toca al jugador 1, se lo dice y al dos que espere
+                if (turnoJugador == 1) {
                     writer1.println(player1Name + ", es tu turno.");
-                    writer2.println("Un momento, es turno de " + player1Name + ".");
-                    int pile = Integer.parseInt(reader1.readLine()) - 1;  // Convertir de 1-3 a 0-2
-                    int tokens = Integer.parseInt(reader1.readLine());
+                    writer2.println("Un momento, es turno de " + player1Name );
+                    //convierte el texto en numero (lo parsea) y le resta uno porque en los arrays se empieza por 0
+                    int fila = Integer.parseInt(reader1.readLine()) - 1;
+                    //lo mismo pero sin -1 porque no hay array
+                    int palos = Integer.parseInt(reader1.readLine());
 
-                    if (tablero[pile].length >= tokens) {
-                        // Quitar las fichas seleccionadas
-                        tablero[pile] = Arrays.copyOf(tablero[pile], tablero[pile].length - tokens);
-                        currentPlayer = 2;
+                    //si hay más palos de los que quiere quitar
+                    if (tablero[fila].length >= palos) {
+                        //quita las fichas, crea una copia del tablero igual, pero con x palos menos (x= palos que quiere quitar el jugador)
+                        tablero[fila] = Arrays.copyOf(tablero[fila], tablero[fila].length - palos);
+                        //ponemos el turno al otro jugador
+                        turnoJugador = 2;
                     } else {
+                        //si elige mas fichas le informamos
                         writer1.println("No puedes quitar más fichas de las que hay en el montón.");
                     }
+                    //lo mismo exacto, pero para el jugador 2
                 } else {
                     writer2.println(player2Name + ", es tu turno.");
                     writer1.println("Un momento, es turno de " + player2Name + ".");
-                    int pile = Integer.parseInt(reader2.readLine()) - 1;  // Convertir de 1-3 a 0-2
-                    int tokens = Integer.parseInt(reader2.readLine());
+                    int fila = Integer.parseInt(reader2.readLine()) - 1;
+                    int palos = Integer.parseInt(reader2.readLine());
 
-                    if (tablero[pile].length >= tokens) {
-                        // Quitar las fichas seleccionadas
-                        tablero[pile] = Arrays.copyOf(tablero[pile], tablero[pile].length - tokens);
-                        currentPlayer = 1;
+                    if (tablero[fila].length >= palos) {
+                        tablero[fila] = Arrays.copyOf(tablero[fila], tablero[fila].length - palos);
+                        //una vez hecho, otra vez al primer jugador
+                        turnoJugador = 1;
                     } else {
                         writer2.println("No puedes quitar más fichas de las que hay en el montón.");
                     }
                 }
 
-                // Mostrar el tablero después de cada movimiento
+                //pongo esto para que despues de cada jugada muestre el tablero
                 showBoard(writer1, writer2, tablero);
 
-                // Comprobar si alguno de los jugadores ha ganado esta partida
+                //creo un boolean para que se haga lo de arriba mientras no se gane
+                //la inicio true y si el juego no se ha acabado la pongo false
                 boolean gameWon = true;
+                //recorro el tablero
                 for (int i = 0; i < tablero.length; i++) {
                     if (tablero[i].length > 0) {
+                        //si aun quedan fichas la pongo false porque el juego no ha acabado
                         gameWon = false;
                         break;
                     }
                 }
 
+                //informo de quien gana si se gana
                 if (gameWon) {
-                    if (currentPlayer == 1) {
+                    if (turnoJugador == 1) {
                         winsP2++;
                         writer1.println("Lo siento "+player1Name+ " has perdido :/");
                         writer2.println("Muy bien "+player2Name+ "has ganado!!");
@@ -108,6 +118,7 @@ public class Main {
                         writer1.println("Muy bien "+player1Name+ "has ganado!!");
                         writer2.println("Lo siento "+player2Name+ " has perdido :/");
                     }
+                    //salimos del while true
                     break;
                 }
             }
@@ -123,7 +134,7 @@ public class Main {
 
         }
 
-        // Final del juego
+        //si han llegado a las partidas que tienen que llegar, se lo digo y se avaba, al que pierde también se lo digo (CÓDIGO FACIL)
         if (winsP1 == 2) {
             writer1.println("Enhorabuena " + player1Name + " como ya has ganado dos partidas, HAS GANADO EL JUEGO !!!");
             writer2.println("Lo siento " +player2Name +" otra vez será, "+player1Name+ " te ha ganado");
@@ -132,13 +143,15 @@ public class Main {
             writer2.println("¡Felicidades, " + player2Name + "! Has ganado el juego.");
         }
 
+        //CERRAMOS TODO
         socket1.close();
         socket2.close();
         serverSocket.close();
     }
 
-    //Función para el tablero
+    //Función para el tablero         estos PrintWriter son para enviar el tablero
     private static void showBoard(PrintWriter writer1, PrintWriter writer2, int[][] tablero) {
+        //esto admito que lo he buscado, sirve para representar el tablero (sirve como un string, pero así no ponemos todo el rato +)
         StringBuilder board = new StringBuilder();
 
         //Enseñar los palods de cada fila, con espacio para verlos bien.
@@ -150,11 +163,11 @@ public class Main {
                 board.append("|   ");  //Para añadir espacio entre los palos
             }
 
-            // Salto de línea para la siguiente fila
+            //cuando acaba de dibujar los palos de una fila salta a la otra
             board.append("\n");
         }
 
-        // Enviar el tablero a ambos jugadores
+        //enseñar el tablaro a los dos jugadores
         writer1.println(board);
         writer2.println(board);
     }
@@ -176,7 +189,7 @@ También se puede poner que si eliges una fila que no tiene palos, directamente 
  */
 
 /*
-Comentado y revisado de la 115 a la 153
+Comentado y revisado all
  */
 
 /*
